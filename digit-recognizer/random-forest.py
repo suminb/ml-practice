b71f72390b032fe0477faf9ff4e6afb1bfac3d8a
Sumin Byeon <suminb@gmail.com>
2018-10-04 20:01:25 +0900

    Multiple commands

--- digit-recognizer/random-forest.py
+++ digit-recognizer/random-forest.py
@@ -2,9 +2,41 @@
 
 import csv
 
+import click
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('output_filename')
+def default(output_filename):
+    X, y = load_data()
+    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+    model = build_model(train_X, train_y)
+    test_data = pd.read_csv('test.csv')
+    predictions = model.predict(test_data)
+    write_output(predictions, output_filename)
+
+
+# Score = 0.93671
+@cli.command()
+@click.argument('output_filename')
+def lines(output_filename):
+    """Captures horizontal and vertical lines as features."""
+    X, y = load_data()
+    X = append_extra_features(X)
+    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+    model = build_model(train_X, train_y)
+    test_data = pd.read_csv('test.csv')
+    test_data = append_extra_features(test_data)
+    predictions = model.predict(test_data)
+    write_output(predictions, output_filename)
 
 
 def load_data(filename='train.csv'):
@@ -13,10 +45,7 @@ def load_data(filename='train.csv'):
     y = train_data['label']
     X = train_data.iloc[:, 1:]
 
-    X = append_extra_features(X)
-
-    # Returns train_X, val_X, train_y, val_y
-    return train_test_split(X, y, test_size=0.25)
+    return X, y
 
 
 def append_extra_features(X):
@@ -28,27 +57,20 @@ def append_extra_features(X):
 
 
 # Default random forest model scored 0.93600 on Kaggle
-def build_model():
-    train_X, val_X, train_y, val_y = load_data()
-
+def build_model(train_X, train_y):
     model = RandomForestClassifier(random_state=0)
     model.fit(train_X, train_y)
 
     return model
 
 
-def write_output(csv_writer, predictions):
-    csv_writer.writerow(['ImageId', 'Label'])
-    for i, p in enumerate(predictions, 1):
-        csv_writer.writerow([i, p])
+def write_output(predictions, filename='output.csv'):
+    with open(filename, 'w') as fout:
+        writer = csv.writer(fout)
+        writer.writerow(['ImageId', 'Label'])
+        for i, p in enumerate(predictions, 1):
+            writer.writerow([i, p])
 
 
 if __name__ == '__main__':
-    model = build_model()
-    test_data = pd.read_csv('test.csv')
-    test_data = append_extra_features(test_data)
-    predictions = model.predict(test_data)
-
-    with open('output.csv', 'w') as fout:
-        writer = csv.writer(fout)
-        write_output(writer, predictions)
+    cli()
